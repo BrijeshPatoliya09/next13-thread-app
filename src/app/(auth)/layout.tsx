771cee646628a9adfaef272955f817cb3c3e7547
Brijesh Patoliya <123737912+BrijeshPatoliya09@users.noreply.github.com>
2023-08-10 18:08:11 +0530

    onboarding from

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,8 +2,14 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import React from "react";
 import "../globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
 
 export default function AuthLayout({
   children,
